e274e0e35aa30243f169ecc511bd27958b51c9e2
John Rodgers <jrodgers@gmail.com>
2023-07-17 19:23:13 -0700

    Initial setup of responsive play area

--- src/Solitaire.js
+++ src/Solitaire.js
@@ -2,26 +2,29 @@ import './Solitaire.css';
 
 function Solitaire() {
   return (
-    <div className="Solitaire">
-      
-      {/* <header className="App-header">
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> M React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header> */}
+    <div id="playarea">
+      <div id="stock">
+        <div id="draw" className="card"></div>
+      </div>
+      <div id="waste">
+      </div>
+      <div id="foundation">
+        <div id="stack1" className="card"></div>
+        <div id="stack2" className="card"></div>
+        <div id="stack3" className="card"></div>
+        <div id="stack4" className="card"></div>
+      </div>
+      <div id="tableau">
+        <div id="tab1" className="card"></div>
+        <div id="tab2" className="card"></div>
+        <div id="tab3" className="card"></div>
+        <div id="tab4" className="card"></div>
+        <div id="tab5" className="card"></div>
+        <div id="tab6" className="card"></div>
+        <div id="tab7" className="card"></div>
+      </div>
+      <div id="menu"></div>
+      <div id="timer"></div>
     </div>
   );
 }
