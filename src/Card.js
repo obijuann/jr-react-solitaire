2f0dbd0e30618290a290e8a019308367b47db99a
John Rodgers <jrodgers@gmail.com>
2023-07-25 20:37:51 -0700

    First pass of card flipping animation

--- src/Card.js
+++ src/Card.js
@@ -29,16 +29,28 @@ class Card extends Component {
     }
   }
 
+  componentWillUnmount() {
+    this.setState({ flipped: false, face: "down" });
+  }
+
   render() {
+    if (this.state.face === "up" && !this.state.flipped) {
+      setTimeout(() => {
+        this.setState({ flipped: true })
+      }, 800);
+    }
+
     return (
-      <div className={`card ${this.state.suit} ${this.state.face === "up" ? "faceup" : ""}`}>
-        <div className="face">
-          <img src={`${process.env.PUBLIC_URL}/cards/fronts/${this.state.suit}_${this.state.rank}.svg`} alt={`${this.state.rank} of ${this.state.suit}`}></img>
-          <span className="rank">{rankMap[this.state.rank] || this.state.rank}</span>
-          <span className="suit" dangerouslySetInnerHTML={{ __html: suitMap[this.state.suit] }}></span>
-        </div>
-        <div className="back">
-          <img src={`${process.env.PUBLIC_URL}/cards/backs/red2.svg`} alt="card"></img>
+      <div className={`card ${this.state.suit} ${this.state.flipped ? "faceup" : ""}`}>
+        <div className="cardinner">
+          <div className="face">
+            <img src={`${process.env.PUBLIC_URL}/cards/fronts/${this.state.suit}_${this.state.rank}.svg`} alt={`${this.state.rank} of ${this.state.suit}`}></img>
+            <span className="rank">{rankMap[this.state.rank] || this.state.rank}</span>
+            <span className="suit" dangerouslySetInnerHTML={{ __html: suitMap[this.state.suit] }}></span>
+          </div>
+          <div className="back">
+            <img src={`${process.env.PUBLIC_URL}/cards/backs/red2.svg`} alt="card"></img>
+          </div>
         </div>
       </div>
     );
