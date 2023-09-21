57fba564d05c3e81d41e3dc91308bcfb3ad6b039
John Rodgers <jrodgers@gmail.com>
2023-09-21 15:56:32 -0700

    First pass of conversion to Vite and TypeScript

--- src/Card.tsx
+++ src/Card.tsx
@@ -53,12 +53,12 @@ export default function Card(props) {
     >
       <div className="card-inner">
         <div className="face">
-          <img src={`${process.env.PUBLIC_URL}/cards/fronts/${props.suit}_${props.rank}.svg`} alt={`${props.rank} of ${props.suit}`} draggable="false"></img>
+          <img src={`/cards/fronts/${props.suit}_${props.rank}.svg`} alt={`${props.rank} of ${props.suit}`} draggable="false"></img>
           <span className="rank">{rankMap[props.rank] || props.rank}</span>
           <span className="suit">{suitMap[props.suit]}</span>
         </div>
         <div className="back">
-          <img src={`${process.env.PUBLIC_URL}/cards/backs/red.svg`} alt="card" draggable="false"></img>
+          <img src={`/cards/backs/red.svg`} alt="card" draggable="false"></img>
         </div>
       </div>
     </div>
---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import './index.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Solitaire from './Solitaire';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Solitaire />
+  </React.StrictMode>
+)
