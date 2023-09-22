0235b432418ae6745edf85c33d0c84cdb7ea5328
John Rodgers <jrodgers@gmail.com>
2023-09-21 19:30:09 -0700

    Updating card UT

--- src/@types/CardComponentProps.d.ts
+++ src/@types/CardComponentProps.d.ts
@@ -1,6 +1,6 @@
 import { CardData } from "./CardData"
 
 export interface CardComponentProps extends CardData {
-    draggable: boolean
+    draggable?: boolean
     offset?: number
 }
--- src/Card.test.tsx
+++ src/Card.test.tsx
@@ -1,37 +1,40 @@
+import { expect, it } from 'vitest';
 import { render, screen } from '@testing-library/react';
 
 import Card from './Card';
 
-test("renders facedown card", () => {
+it("renders facedown card", () => {
     // Arrange + Act
-    render(<Card rank="10" suit="hearts" face="down" />);
+    render(<Card rank="10" suit="hearts" face="down"/>);
 
     // Assert
     const cardElement = screen.getByTestId("card");
-    expect(cardElement).toBeInTheDocument();
+    expect(cardElement).toBeDefined();
     expect(cardElement.className).toEqual("card ");
-    expect(JSON.parse(cardElement.getAttribute("data-carddata"))).toEqual({ rank: "10", suit: "hearts", face: "down" });
-    expect(cardElement.getAttribute("draggable")).toEqual("false");
+    const cardData = cardElement.getAttribute("data-carddata") || "";
+    expect(JSON.parse(cardData)).toEqual({ rank: "10", suit: "hearts", face: "down" });
+    expect(cardElement.getAttribute("draggable")).toBeFalsy();
 });
 
-test("renders face up card", () => {
+it("renders face up card", () => {
     // Arrange + Act
-    render(<Card rank="10" suit="hearts" face="up" />);
+    render(<Card rank="10" suit="hearts" face="up" draggable={true} />);
 
     // Assert
     const cardElement = screen.getByTestId("card");
-    expect(cardElement).toBeInTheDocument();
+    expect(cardElement).toBeDefined();
     expect(cardElement.className).toEqual("card hearts faceup");
-    expect(JSON.parse(cardElement.getAttribute("data-carddata"))).toEqual({ rank: "10", suit: "hearts", face: "up" });
-    expect(cardElement.getAttribute("draggable")).toEqual("true");
+    const cardData = cardElement.getAttribute("data-carddata") || "";
+    expect(JSON.parse(cardData)).toEqual({ rank: "10", suit: "hearts", face: "up", draggable: true });
+    expect(cardElement.getAttribute("draggable")).toBeTruthy();
 });
 
-test("renders offset card", () => {
+it("renders offset card", () => {
     // Arrange + Act
-    render(<Card rank="10" suit="hearts" face="up" offset="12" />);
+    render(<Card rank="10" suit="hearts" face="up" offset={12} />);
 
     // Assert
     const cardElement = screen.getByTestId("card");
-    expect(cardElement).toBeInTheDocument();
+    expect(cardElement).toBeDefined();
     expect(cardElement.style.transform).toEqual("translateY(12vh)");
 });
