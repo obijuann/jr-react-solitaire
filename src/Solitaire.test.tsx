da3f0e46a43720bf6c4136497f54683a8b3c67a7
John Rodgers <jrodgers@gmail.com>
2023-09-26 13:34:09 -0700

    Unit test fixes

--- src/Solitaire.test.tsx
+++ src/Solitaire.test.tsx
@@ -1,12 +1,10 @@
 import '@testing-library/jest-dom/vitest';
 
+import { act, render, screen } from '@testing-library/react';
 import { expect, it } from 'vitest';
-import { render, screen } from '@testing-library/react';
 
 import Solitaire from './Solitaire';
-
-// Add a polyfill for structured clone
-global.structuredClone = (val) => JSON.parse(JSON.stringify(val))
+import { publish } from './Events';
 
 it('renders the play area', () => {
   // Arrange + Act
@@ -17,8 +15,72 @@ it('renders the play area', () => {
   expect(playArea).toBeInTheDocument();
   expect(playArea.querySelector("#stock")).toBeInTheDocument();
   expect(playArea.querySelector("#draw")).toBeInTheDocument();
+  expect(playArea.querySelector("#draw")?.childNodes.length).toEqual(0);
   expect(playArea.querySelector("#waste")).toBeInTheDocument();
+  expect(playArea.querySelector("#waste")?.childNodes.length).toEqual(0);
   expect(playArea.querySelector("#foundation")).toBeInTheDocument();
+  expect(playArea.querySelector("#foundation")?.childNodes.length).toEqual(4);
   expect(playArea.querySelector("#tableau")).toBeInTheDocument();
+  expect(playArea.querySelector("#tableau")?.childNodes.length).toEqual(7);
   expect(playArea.querySelector("#menu")).toBeInTheDocument();
+});
+
+it('Deals the cards when a new game event is published', () => {
+  // Arrange
+  render(<Solitaire />);
+
+  // Act
+  act(() => {
+    // Wrapped in an act call since this is render-affecting
+    publish("newGame");
+  });
+
+  // Assert
+  const playArea = screen.getByTestId("play-area");
+  expect(playArea).toBeInTheDocument();
+  expect(playArea.querySelector("#draw")).toBeInTheDocument();
+  expect(playArea.querySelector("#draw")?.childNodes.length).toEqual(24);
+  expect(playArea.querySelector("#waste")).toBeInTheDocument();
+  expect(playArea.querySelector("#waste")?.childNodes.length).toEqual(0);
+  expect(playArea.querySelector("#foundation")).toBeInTheDocument();
+  playArea.querySelector("#foundation")?.childNodes.forEach(foundationPile => {
+    expect(foundationPile.childNodes.length).toEqual(0);
+  });
+  expect(playArea.querySelector("#tableau")).toBeInTheDocument();
+  playArea.querySelector("#tableau")?.childNodes.forEach((tableauPile, pileIndex) => {
+    expect(tableauPile.childNodes.length).toEqual(pileIndex + 1);
+  });
+  expect(playArea.querySelector("#menu")).toBeInTheDocument();
+});
+
+it('Removes cards from the playfield when the game is exited', () => {
+  // Arrange
+  render(<Solitaire />);
+
+  // Act
+  act(() => {
+    // Wrapped in an act call since this is render-affecting
+    publish("newGame");
+  });
+
+  // Assert
+  const playArea = screen.getByTestId("play-area");
+  expect(playArea).toBeInTheDocument();
+  expect(playArea.querySelector("#draw")).toBeInTheDocument();
+  expect(playArea.querySelector("#draw")?.childNodes.length).toEqual(24);
+
+  // Act
+  act(() => {
+    // Wrapped in an act call since this is render-affecting
+    publish("exitGame");
+  });
+
+  expect(playArea.querySelector("#draw")?.childNodes.length).toEqual(0);
+  expect(playArea.querySelector("#waste")?.childNodes.length).toEqual(0);
+  playArea.querySelector("#foundation")?.childNodes.forEach(foundationPile => {
+    expect(foundationPile.childNodes.length).toEqual(0);
+  });
+  playArea.querySelector("#tableau")?.childNodes.forEach(tableauPile => {
+    expect(tableauPile.childNodes.length).toEqual(0);
+  });
 });
