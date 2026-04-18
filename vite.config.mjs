33ea8091398a5b4a5ea8cc4d307cb5b803a05274
John Rodgers <jrodgers@gmail.com>
2026-04-17 19:33:15 -0700

    Updating other package versions. Removing vite-tsconfig-paths

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,9 +1,12 @@
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig, resolve } from 'vite';
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react()],
+  resolve: {
+    // Enables automatic TSConfig path mapping
+    tsconfigPaths: true
+  },
   test: {
     globals: true,
     environment: 'jsdom',
